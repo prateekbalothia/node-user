e1d8a2259935f02e756afb809dedf78c2736c36a
Prateek balothia <prateekbalothia.p@gmail.com>
2025-08-14 15:47:03 +0530

    bcrypt

--- controller/userController.js
+++ controller/userController.js
@@ -1,23 +1,39 @@
 const user = require('../models/usermodel');
+const bcrypt = require('bcrypt')
 
 
-
-
-const addUser=async (req,res)=>{
-    const {name, password, email} = req.body;
-    if(!name || !password || !email){
-        console.log("fields required!!")
-    }
-    const newUser = new user({name, password, email});
-    const result = await newUser.save();
-    console.log(result);
-    res.send(result);
-    console.log(req.body);
-}
 
 const getuser=async (req,res) => {
     let data = await user.find();
     res.send(data)
 }
 
-module.exports = { addUser,getuser }
+const addUser=async (req,res)=>{
+    const {name, password, email} = req.body;
+    if(!name || !password || !email){
+        console.log("fields required!!")
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new user({name, password:hashedPassword, email});
+    const result = await newUser.save();
+    console.log(result);
+    res.send(result);
+    console.log(req.body);
+}
+
+const updateUser = async (req,res) => {
+        let data = await user.updateOne(
+        req.params,
+        {$set: req.body}
+    )
+    res.send(data)
+}
+
+const deleteUser = async (req,res) => {
+    console.log(req.params);
+    let data = await user.deleteOne(req.params);
+    res.send(data);
+}
+
+
+module.exports = { addUser,getuser,updateUser,deleteUser }
--- route/api.js
+++ route/api.js
@@ -2,9 +2,10 @@ const express=require('express')
 const app=express()
 const router=express.Router()
 
+const {addUser,getuser,updateUser,deleteUser}=require('../controller/userController')
 
-const {addUser,getuser}=require('../controller/userController')
-
-router.post('/', addUser)
 router.get("/find-user",getuser)
+router.post('/add-user', addUser)
+router.put('/update-user/:_id', updateUser)
+router.delete('/delete-user/:_id',deleteUser)
 module.exports=router
